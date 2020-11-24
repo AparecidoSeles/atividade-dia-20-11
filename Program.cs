e5f8c40be9919d86589695c50f0a485e7ee73654
junior <you@example.com>
2020-11-23 22:21:59 -0300

    conclusão

--- Program.cs
+++ Program.cs
@@ -1,55 +1,85 @@
 ﻿using System;
 
-namespace atividade_dia_20_11
+namespace reforco
 {
     class Program
-    {      
-        
-                //Faça um programa que receba o nome e quatro notas de 10 alunos e armazene essa notas em vetores.
-                // calcule e mostre:
-                //A. a media do aluno
-                //B. A quanridade de aprovados( media >= 7)
-                //C. Quanaridade de reprovados( media < 7)
-                //media geral dos alunos
+    {
+        static float CalcularMedia(float[] numeros){
+
+            float soma = 0;
+
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                // soma = soma + numeros[i];
+                soma += numeros[i];
+            }
+            
+            float calculo = soma / numeros.Length;
+
+            return calculo;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Atividade alunos aprovados e reprovados");
 
-            string[] nomes = new string[10];
-            float nota1º ;
-            float nota2º ;
-            float nota3º ;
-            float nota4º ;
-            
-            for (var contador = 0; contador < 10; contador++)
-            {
-                Console.WriteLine("Digite seu nome");
-                nomes[contador] = Console.ReadLine();
-                for (var i = 0; i < 4; i++)
+            Console.ForegroundColor = ConsoleColor.White;
+            /*
+                array - aplicação
+                foreach
+                função/método            
+            */
+            string[] nomes       = new string[4];
+            float [] mediaAluno  = new float[3];
+            float [] notas       = new float[4];
+
+            // 0
+            // Mateus
+            // 7.8
+
+            // Laço contado
+            // O laço FOR já tem na sua sintaxe elementos para contar números
+            // Opções de incremento:
+            // i++       (adiciona o seu valor mais um)
+            // i = i + 1 (adiciona o seu valor mais um)
+            // i += i     (adiciona seu valor mais ele mesmo ex: 1 + 1 = 2 (2 + 2) = 4 e assim por dainte )
+            for(int i = 0; i < 4; i++){
+
+                Console.WriteLine("Escreva um nome : ");
+                nomes[i] = Console.ReadLine();
+
+                // deixar a var diferente do laço anterior
+                // caso deixe dara erro
+                // e seguirá o var de  cima
+                for (var x = 0; x < 4; x++)
                 {
-                    Console.WriteLine("Digite 1º nota");
-                    nota1º= float.Parse(Console.ReadLine());
+                    // Concatenação
+                    Console.WriteLine("Digite a "+(x+1)+"° nota: ");
+                    notas[x] = float.Parse(Console.ReadLine());
+                }
 
-                    Console.WriteLine("Digite 2º nota");
-                    nota2º = float.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Digite 3º nota");
-                    nota3º = float.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Digite 4º nota");
-                    nota4º = float.Parse(Console.ReadLine());
-
-                }   
-                
+                mediaAluno[i] = CalcularMedia(notas);
 
             }
-            
-        }
 
-        // foreach (var item in collection)
-        // {
-            
-        // }
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            // Laço condicionais
+            // WHILE / DO WHILE / FOREACH
+            foreach(string nomeAvulso in nomes)
+            {
+                Console.WriteLine(nomeAvulso);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            //foreach = Mostrar médias e nomes em laços de repetição
+            //Array.Length = tamanho do array, caso eu não saiba (dinâmica)
+            for (var i = 0; i < nomes.Length ; i++)
+            {
+                // Interpolação
+                Console.WriteLine($"Nome : {nomes[i]} - Média : {mediaAluno[i]} ");
+            }  
+
+        }
     }
 }
